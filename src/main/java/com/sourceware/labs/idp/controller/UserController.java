1dc6f968e2a17d5e5feb437ba1257a0696439246
Robert <robert.s.forristall@gmail.com>
2024-12-10 20:47:46 -0500

    feat: add logger with example

--- src/main/java/com/sourceware/labs/idp/controller/UserController.java
+++ src/main/java/com/sourceware/labs/idp/controller/UserController.java
@@ -2,6 +2,8 @@ package com.sourceware.labs.idp.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,8 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class UserController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
 	private final UserRepository repo;
 	
@@ -25,11 +29,13 @@ public class UserController {
 	
 	@GetMapping("/user")
 	List<User> all() {
+		LOGGER.info("in GET /user ...");
 		return repo.findAll();
 	}
 	
 	@PostMapping("user")
 	User newUser(@RequestBody User newUser, HttpServletResponse response) {
+		LOGGER.info("in POST /user ...");
 		User user = repo.save(newUser);
 		response.setStatus(HttpStatus.CREATED.value());
 		return user;
