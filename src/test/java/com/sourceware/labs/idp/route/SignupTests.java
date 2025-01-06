578d530ec590bf0db0d85d049048d90d55c86fc8
Robert <robert.s.forristall@gmail.com>
2025-01-06 12:33:44 -0500

    test: add missing tests for signup route data

--- src/test/java/com/sourceware/labs/idp/route/SignupTests.java
+++ src/test/java/com/sourceware/labs/idp/route/SignupTests.java
@@ -2,6 +2,7 @@ package com.sourceware.labs.idp.route;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Date;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -75,6 +76,168 @@ public class SignupTests extends BaseIdpApplicationTests{
 		SignupData signupData = TestData.getTestSignupData();
 		ResponseEntity<String> result = issueRequest(signupData);
 		assertRestErrorsEqual(result, 409, 5, "Error: A user with the provided email already exists");
+	}
+	
+	@Test
+	public void catchPasswordIsNull() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setPassword(null);
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 6, "Error: User's password must be defined");
+	}
+	
+	@Test
+	public void catchPasswordIsBlank() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setPassword("");
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 6, "Error: User's password must be defined");
+	}
+	
+	@Test
+	public void catchFirstNameIsNull() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setFirstName(null);
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 6, "Error: User's first name must be defined");
+	}
+	
+	@Test
+	public void catchFirstNameIsBlank() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setFirstName("");
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 6, "Error: User's first name must be defined");
+	}
+	
+	@Test
+	public void catchLastNameIsNull() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setLastName(null);
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 6, "Error: User's last name must be defined");
+	}
+	
+	@Test
+	public void catchLastNameIsBlank() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setLastName("");
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 6, "Error: User's last name must be defined");
+	}
+	
+	@Test
+	public void catchDobIsNull() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setDob(null);
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 2, "Error: User is not old enough");
+	}
+	
+	@Test
+	public void catchUserIsTooYoung() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setDob(new Date());
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 2, "Error: User is not old enough");
+	}
+	
+	@Test
+	public void catchSecurityQuestion1Null() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setSq1(null);
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 6, "Error: Security question 1 must be defined");
+	}
+	
+	@Test
+	public void catchSecurityQuestion1Blank() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setSq1("");
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 6, "Error: Security question 1 must be defined");
+	}
+	
+	@Test
+	public void catchSecurityQuestion2Null() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setSq2(null);
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 6, "Error: Security question 2 must be defined");
+	}
+	
+	@Test
+	public void catchSecurityQuestion2Blank() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setSq2("");
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 6, "Error: Security question 2 must be defined");
+	}
+	
+	@Test
+	public void catchSecurityAnswer1Null() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setSa1(null);
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 6, "Error: Security answer 1 must be defined");
+	}
+	
+	@Test
+	public void catchSecurityAnswer1Blank() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setSa1("");
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 6, "Error: Security answer 1 must be defined");
+	}
+	
+	@Test
+	public void catchSecurityAnswer2Null() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setSa2(null);
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 6, "Error: Security answer 2 must be defined");
+	}
+	
+	@Test
+	public void catchSecurityAnswer2Blank() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setSa2("");
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 6, "Error: Security answer 2 must be defined");
+	}
+	
+	@Test
+	public void catchRecoveryEmailValid() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setRecoveryEmail("testing");
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 3, "Error: User's recovery email is not valid");
+	}
+	
+	@Test
+	public void catchRecoveryPhoneValid() {
+		SignupData signupData = TestData.getTestSignupData();
+		signupData.setEmail("test2@test.com");
+		signupData.setRecoveryPhone("123");
+		ResponseEntity<String> result = issueRequest(signupData);
+		assertRestErrorsEqual(result, 400, 4, "Error: User's recovery phone number is not valid");
 	}
 	
 	private ResponseEntity<String> issueRequest(SignupData data) {
